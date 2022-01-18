cda07413e8a82b61ca09c5543702824e7a6e4333
Gympiecuddles <schembri.y93@gmail.com>
2022-01-18 08:03:02 -0500

    Google library

--- src/Containers/Hero/ContactHero.js
+++ src/Containers/Hero/ContactHero.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './ContactHero.css'
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 function ContactHero() {
+    
+
     return (
-        <div>
-            <a href="https://goo.gl/maps/CKHMrdoeWrvUZmJFA">Address</a>
-            <a href="tel:5614602227">(561) 460-2227</a>
-        </div>
+        <section>
+            <article>
+                <a className="address" href="https://goo.gl/maps/mYUFn9T3AVZG9AWS7">861 N Military Trl</a>
+                <a className='phone' href="tel:5614602227">(561) 460-2227</a>
+                <p>Hours:</p>
+                <ul className='Hours'>
+                    <li>Mon-Fri: 8am-7pm</li>
+                    <li>Saturday: Closed</li>
+                    <li>Sunday: 8am-7pm</li>
+                </ul> 
+            </article>
+            <div id="map">e</div>
+        </section>
     )
 }
 
